2c5b42d7edc717a78f5e5d8cc05db91c6a36a90f
18830974630 <691577405@qq.com>
2020-11-10 19:57:17 +0800

    hsf

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
-<<<<<<< HEAD
-    <router-view></router-view>
-=======
     <Header></Header>
     <Navigation></Navigation>
->>>>>>> 60c170bf9af799e5c079fbbe9b3063bf48357d43
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ Vue.config.productionTip = false
 
 //引入element-ui
 import '../theme/index.css' //自定义主题
-<<<<<<< HEAD
 import {
     Button,
     Select,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    Tooltip,
+    Menu,
+    Submenu,
+    MenuItem,
     Card,
     Progress,
     Row,
@@ -16,18 +22,6 @@ import {
     Avatar
 } from 'element-ui';
 //注册element组件
-Vue.use(Button)
-Vue.use(Select)
-Vue.use(Card)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Progress)
-Vue.use(Avatar)
-=======
-import {Button, Select, Dropdown, DropdownMenu, DropdownItem, Tooltip, Menu, Submenu, MenuItem} from 'element-ui';
-//注册element组件
-Vue.use(Button)
-Vue.use(Select)
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
@@ -35,7 +29,13 @@ Vue.use(Tooltip)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
->>>>>>> 60c170bf9af799e5c079fbbe9b3063bf48357d43
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Progress)
+Vue.use(Avatar)
 
 //引入路由器
 import router from "@/router"
