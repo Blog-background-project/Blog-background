9e2b6cf5b24239b5e0a8ec8345356e51e802746f
aishuaikang <2506377990@qq.com>
2020-11-11 09:09:16 +0800

    修改vuex的模块化

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,14 @@ import Vuex from "vuex"
 
 Vue.use(Vuex)
 
-import aishuaikang from "./modules/aishuaikang"
-import suoxiaoyu from "./modules/suoxiaoyu";
-import tanzhiguo from "./modules/tanzhiguo";
-import huangshaofei from "./modules/huangshaofei";
-import baohua from "./modules/baohua";
-import lvpengchao from "./modules/lvpengchao";
+import Home from "@/store/modules/Home";
+import FileManagement from '@/store/modules/FileManagement'
+import Categorylist from "@/store/modules/Categorylist"
 
 export default new Vuex.Store({
     modules: {
-        aishuaikang,
-        suoxiaoyu,
-        tanzhiguo,
-        huangshaofei,
-        baohua,
-        lvpengchao,
+        Home,
+        FileManagement,
+        Categorylist
     }
 })
