b15dfd0b56193e3da72dd146761c1528a532e00f
aishuaikang <2506377990@qq.com>
2020-11-12 10:23:33 +0800

    ask

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,26 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter)
 import routes from "./modules"
 
-export default new VueRouter({
+
+let router = new VueRouter({
     mode: "hash",
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') {
+        next()
+        var tokenStr = sessionStorage.getItem('OPENTOKEN_KEY') || localStorage.getItem("OPENTOKEN_KEY")
+    }
+    if (!tokenStr) {
+        console.log("yunxingle1")
+
+        next()
+    } else {
+        console.log("yunxingle")
+        next('/login')
+    }
+})
+
+export default router
+
